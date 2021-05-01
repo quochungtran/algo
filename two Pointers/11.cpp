fb2f56ad1d0b2f47c94a7f5e408ea981c18ec4a3
quochungtran <quochungtr99@gmail.com>
2021-05-01 18:15:09 +0200

    LeetCode 11 : Container with most water

---
+++ two Pointers/11.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int n = height.size();
+
+        int start = 0;
+        int end = n - 1;
+        int maxAre = 0;
+
+        while (start < end)
+        {
+            int h = min(height[start], height[end]);
+            int width = end - start;
+
+            maxAre = max(maxAre, h * width);
+
+            if (height[start] < height[end])
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+
+        return maxAre;
+    }
+};
