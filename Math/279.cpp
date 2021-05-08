e687c25cccb667da2001745b9e4be167f7dd068a
quochungtran <quochungtr99@gmail.com>
2021-05-08 22:13:57 +0200

    LeetCode 279 Perfect Squares

---
+++ Math/279.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        if (n <= 0)
+            return 0;
+
+        int dp[n + 1];
+
+        for (int k = 0; k < n + 1; k++)
+        {
+            dp[k] = INT_MAX;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j * j <= i; j++)
+            {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+};
