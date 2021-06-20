fe913e0b67fc434c00e1e71176cb13903cb22fff
TRAN <quochungtr99@gmail.com>
2021-06-20 16:48:39 +0200

    LeetCode 378 : Kth Smallest Ele in a sorted Matrix

---
+++ Heap/378.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int kthSmallest(vector<vector<int>> &matrix, int k)
+    {
+        priority_queue<int> maxHeap;
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                maxHeap.push(matrix[i][j]);
+                while (maxHeap.size() > k)
+                {
+                    maxHeap.pop();
+                }
+            }
+        }
+
+        return maxHeap.top();
+    }
+};
